3e143671ca738243ef81a6a929ee1dc386526deb
Moataz <moataz.wmm@gmail.com>
2025-07-26 02:19:13 +0300

    refactor:primary color app and add splash screen

--- lib/config/theme/colors.dart
+++ lib/config/theme/colors.dart
@@ -1,7 +1,7 @@
 import 'dart:ui';
 
 class AppColors {
-  static Color primary =const Color(0xffFE8C00);
+  static Color primary =const Color(0xffEC2578);
   static Color offWhite =const Color(0xffFFEEDA);
   static Color white =const Color(0xFFFFFFFF);
   static Color secondary =const Color(0xFF000000);
